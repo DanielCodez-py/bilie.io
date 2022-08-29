4e7c3f39b54396bb00c871ab232f23b927d25479
Danny Orajiaku <dannydoczhero@gmail.com>
2022-08-29 10:15:51 -0400

    This is the commit after dev on the navbar

---
+++ Re-bigspaceship/styles/script.js
@@ -0,0 +1,7 @@
+const ham = document.querySelector('.hamIcon');
+const nav = document.querySelector('#navBar');
+
+toggle = () => {
+    ham.classList.toggle("active");
+    nav.classList.toggle("active");
+}
