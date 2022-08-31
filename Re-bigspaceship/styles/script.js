4baf7caa9b5f273717321a8f2fbfd8098954e2b1
Danny Orajiaku <dannydoczhero@gmail.com>
2022-08-30 20:55:54 -0400

    Finished designing the navBar

--- Re-bigspaceship/styles/script.js
+++ Re-bigspaceship/styles/script.js
@@ -1,7 +1,10 @@
 const ham = document.querySelector('.hamIcon');
 const nav = document.querySelector('#navBar');
+const logo = document.querySelector('.logo');
 
-toggle = () => {
+ham.onclick =  toggle = () => {
     ham.classList.toggle("active");
     nav.classList.toggle("active");
+    logo.classList.toggle("active");
 }
+
